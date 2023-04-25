4de5b4b60c248cf24c225473671e4730817645e3
jin <syuban666@gmail.com>
2023-04-25 14:03:06 +0900

    header&backgroundImage setting

---
+++ src/components/common/header/Header.tsx
@@ -0,0 +1,11 @@
+import * as styled from "../../styles/common/header/header";
+
+const Header = () => {
+    return (
+        <styled.Wrapper>
+            Header
+        </styled.Wrapper>
+    );
+};
+
+export default Header;
